c7682bf307588478282882efd3023ae5c3939c3a
Aslan <Aslan@yo.com>
2026-09-24 21:39:15 -0600

    Segundo intento de insertar carpeta Interfaz

---
+++ Interfaz/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InterfazUno
+{
+    public partial class Form1 : Form
+    {
+        Font letra = new Font("Arial", 14, FontStyle.Bold);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Uno";
+            Image imgFondo = CargarImagen("r.jpg", new Size(2200, 1440), "Recursos");
+            this.WindowState = FormWindowState.Maximized;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.MinimizeBox = false;
+            this.CenterToScreen();
+            this.BackgroundImage = imgFondo;
+
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+            
+            Button play = new Button();
+            play.Text = "Jugar";
+            play.Font = new Font("Cabin Bold", 60, FontStyle.Bold);
+            play.ForeColor = Color.White;
+            play.BackColor = Color.Red;
+            play.Size = new Size(350, 125);
+            play.Location = new Point((this.Width - play.Width) / 2, 750);
+            play.FlatAppearance.BorderSize = 4;
+            play.FlatAppearance.BorderColor = Color.White;
+            play.FlatStyle = FlatStyle.Flat;
+            play.Cursor = Cursors.Hand;
+            play.Click += Play_Click;
+            this.Controls.Add(play);
+        }
+
+        private Image CargarImagen(string nombreArchivo, Size tamaño, String ruta_d)
+        {
+            string ruta = Path.Combine(Application.StartupPath, ruta_d, nombreArchivo);
+            if (!File.Exists(ruta))
+                throw new FileNotFoundException($"No se encontró la imagen: {ruta}");
+            Image original = Image.FromFile(ruta);
+            return new Bitmap(original, tamaño);
+        }
+
+        private void Play_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();
+            this.Hide();
+            form2.FormClosed += (s, args) => this.Close();
+        }
+    }
+}
---
+++ Interfaz/Form2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InterfazUno
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            this.Text = "Uno";
+            this.WindowState = FormWindowState.Maximized;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.MinimizeBox = false;
+            this.CenterToScreen();
+            Image fondo = CargarImagen("juego_fondo.jpg", new Size(736, 368), "Recursos");
+            this.BackgroundImage = fondo;
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+            RepartirCartas();
+
+        }
+        private Image CargarImagen(string nombreArchivo, Size tamaño, String ruta_d)
+        {
+            string ruta = Path.Combine(Application.StartupPath, ruta_d, nombreArchivo);
+            if (!File.Exists(ruta))
+                throw new FileNotFoundException($"No se encontró la imagen: {ruta}");
+            Image original = Image.FromFile(ruta);
+            return new Bitmap(original, tamaño);
+        }
+        private void RepartirCartas()
+        {
+            string[] usados = new string[];
+            string[] archivos = Directory.GetFiles("C:\Users\aslan\source\repos\InterfazUno\InterfazUno\bin\Debug\Cartas");
+            if (archivos.Length > 0) {
+                int ran_indice = Random.Shared.Next(archivos.Length);
+                string select = archivos[ran_indice];
+                Console.WriteLine(select);
+                /*Label imagen = new Label();
+                imagen.Image = prueba;
+                this.Controls.Add(imagen);*/
+            }
+            else
+            {
+                Console.WriteLine("La carpeta esta vacia");
+            }
+
+        }
+    }
+}
